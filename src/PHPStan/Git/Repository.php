7664ec6e8b74d927f50584d6cb5740a7c1a33788
nazonohito51 <nazonohito51@gmail.com>
2019-06-11 01:02:29 +0900

    update Repository

--- src/PHPStan/Git/Repository.php
+++ src/PHPStan/Git/Repository.php
@@ -16,8 +16,12 @@ class Repository extends GitRepository
 
     public function getAddedFiles(): array
     {
+        if (is_null($mergeBase = $this->getMergeBase())) {
+            throw new \RuntimeException();
+        }
+
         try {
-            $files = $this->extractFromCommand('git diff --diff-filter=A --name-only master...HEAD', function ($value) {
+            $files = $this->extractFromCommand("git diff --diff-filter=A --name-only {$mergeBase}...HEAD", function ($value) {
                 return Path::join($this->getRepositoryPath(), trim($value));
             });
 
@@ -29,5 +33,60 @@ class Repository extends GitRepository
         }
 
         return [];
+    }
+
+    public function getModifiedFiles(): array
+    {
+        if (is_null($mergeBase = $this->getMergeBase())) {
+            throw new \RuntimeException();
+        }
+
+        try {
+            $files = $this->extractFromCommand("git diff --diff-filter=M --name-only {$mergeBase}...HEAD", function ($value) {
+                return Path::join($this->getRepositoryPath(), trim($value));
+            });
+
+            if (!empty($files)) {
+                return $files;
+            }
+        } catch (GitException $e) {
+            // handle exceptions
+        }
+
+        return [];
+    }
+
+    public function getFileContent(string $file, string $commitHash = null): string
+    {
+        try {
+            $commitHash = $commitHash ?? 'HEAD';
+            $file = Path::makeRelative($file, $this->getRepositoryPath());
+            $content = $this->extractFromCommand("git show {$commitHash}:{$file}", function ($value) {
+                return $value;
+            });
+
+            return implode("\n", $content);
+        } catch (GitException $e) {
+            // handle exceptions
+        }
+
+        throw new \RuntimeException();
+    }
+
+    public function getMergeBase(): string
+    {
+        try {
+            $commitHash = $this->extractFromCommand('git show-branch --merge-base test HEAD', function ($value) {
+                return trim($value);
+            });
+
+            if (isset($commitHash[0])) {
+                return $commitHash[0];
+            }
+        } catch (GitException $e) {
+            // handle exceptions
+        }
+
+        throw new \RuntimeException();
     }
 }
